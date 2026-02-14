5b2b7c33fff0477a12816828acf1986a3a773db0
Elena Ostapova <elenaostapova@MacBook-Pro-Elena.local>
2026-02-14 19:44:13 +0200

    Added Task1, Task2 and Task3  functions (YelienaOstapova)

---
+++ Program.cs
@@ -0,0 +1,133 @@
+using System.Linq; //ця штука як max(a, b) в пайтоні. якщо тобі не треба то не звертай увагу
+// mainS = рядки що обєднані у один рядок, назва усюди для нього однакова
+// dict = словник створений із mainS. слово - ксть слів
+
+namespace ConsoleApp2;
+class Program
+{
+    static string GetInputText() //тут просто повертає один рядок куди ми обєднали усі
+    {
+        string mainS = "";
+        string input = " ";
+        while (input != "")
+        {
+            input = Console.ReadLine();
+            mainS += " " + input;
+        }
+        //увага! просто рядок
+        return mainS; //поветрає стрінг, в оголощенні функції тип як раз стрінг
+    }
+
+    static Dictionary<string, int> GetDictionary(string mainS)//повертає словник, приймає строку. зробила окремо бо мб знадобиться
+    {
+        string[] words = mainS.Split(new[] { ' ', '.', ',','!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+        //зверху імба, із рядка залишаються лише слова і нічого більше
+        
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+
+        foreach (string word in words)
+        {
+            string lowWord = word.ToLower(); //робить слово з малої
+            if (dict.ContainsKey(lowWord))
+            {
+                dict[lowWord]++;
+            }
+            else
+            {
+                dict[lowWord] = 1; 
+            }
+        }
+        return dict; 
+    }
+    
+    
+    //тут далі йдуть функції по таскам
+    static void Task1(string mainS) //до завдання1, нічо не дає тому void, бере в себе меінС як стрінг
+    {
+        int countOfSpaces = 0;
+        int countOfWords = 0;
+        int countOfSentence = 0;
+
+        for (int i = 0; i < mainS.Length; i++)
+        {
+            if (mainS[i] == ' ')
+            {
+                countOfSpaces++;
+            }
+            if (mainS[i] == '.')
+            {
+                countOfSentence++;
+            } 
+        }//далі тупо аналіз шо ксть пробілів == ксть слів-1, ксть точок == ксть речень і т.д
+        Console.WriteLine("");
+        Console.WriteLine("TASK 1");
+        Console.WriteLine($"Count of sentense: {countOfSentence}");
+        Console.WriteLine($"Count of words:, {countOfSpaces-1}");
+        Console.WriteLine($"Count of symbols:  {mainS.Length - countOfSpaces}");
+    }
+
+    static void Task2(Dictionary<string, int> dict)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("TASK 2");
+        int countOfDict = dict.Count;
+        if (countOfDict < 5)
+        {
+            foreach (var pair in dict)
+            {
+                Console.WriteLine($"Most popular word: {pair.Key} - {pair.Value} times");
+            }
+            return;//нічо не повертає просто з функції виходить
+        }
+        
+        string[] words = new string[countOfDict];//дістає із словника ключ і кладе в массив
+        int[] counts = new int[countOfDict];//тут так само але із значеннями
+        
+        int head = 0;
+        foreach (var pair in dict)
+        {
+            words[head] = pair.Key;
+            counts[head] = pair.Value;
+            head++;
+        }
+        
+        int max = 0;
+        int indexOfMax = -1;
+        for (int i = 0; i < 5; i++)//тупо знаходить максимум і його індекс, виводить слово і занулює теперішній максимум щоб не заважав
+        {
+            max = counts.Max();
+            indexOfMax = Array.IndexOf(counts, max);
+            Console.WriteLine($"{i+1}st most popular word: {words[indexOfMax]} - {max} times");
+            counts[indexOfMax] = 0;
+
+        }
+        return;
+    }
+
+    static void Task3(Dictionary<string, int> dict) //нічо не повертає, бере dict
+    {
+        float totalLength = 0;//загальна ксть букв
+        float countOfWords = 0;//на шо ділити загальну ксть букв щоб вийшло середнє
+
+        foreach (var pair in dict)
+        {
+            totalLength += pair.Key.Length;
+            countOfWords++;
+        }
+        Console.WriteLine("");
+        Console.WriteLine("TASK 3");
+        Console.WriteLine($"Average word lenght: {totalLength / countOfWords}");
+    }
+    
+    
+    static void Main()
+    {
+        string mainS = GetInputText(); // тут у маінС строка, зчитуємо ж лише один раз
+        Dictionary<string, int> dict = GetDictionary(mainS); //на всяк випадок окремо зробила словник, мб тобі знадобиться
+        
+        Task1(mainS);
+        Task2(dict);
+        Task3(dict);
+    }
+      
+}
